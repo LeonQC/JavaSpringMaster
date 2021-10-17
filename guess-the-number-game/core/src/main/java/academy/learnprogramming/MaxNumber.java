f95e326dbb5fb20de77ab951d5ff98a0df09b1c3
Tianhao Wang <tvw5292@gmail.com>
2021-10-17 13:11:08 -0400

    Complete Section 5: New Spring - Multi Module Spring Project

---
+++ guess-the-number-game/core/src/main/java/academy/learnprogramming/MaxNumber.java
@@ -0,0 +1,14 @@
+package academy.learnprogramming;
+
+import javax.inject.Qualifier;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Qualifier
+public @interface MaxNumber {
+
+}
